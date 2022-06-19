67f183541d11dc5e1fe93b24445f60c606cfbb3e
briankc804 <briankc804@gmail.com>
2022-06-19 12:44:06 +0300

    update changes

--- myhood/views.py
+++ myhood/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+posts = [
+    {
+    'author':'Brian',
+    'title':'Passionate in Coding',
+    'content':'The best site for coding',
+    'date_posted':'June 19, 2022',
+    },
+    
+    {'author':'Tonia',
+    'title':'Passionate in swiming',
+    'Content':'The best swimmer',
+    'date_posted':'June 19, 2022',}
+]
+
+def home(request):
+    context = {
+        'posts':posts
+    }
+    return render(request, 'rubic/home.html', context)
+
+def about(request):
+    return render(request, 'rubic/about.html', {'title': 'About'})
